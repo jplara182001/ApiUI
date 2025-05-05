a585463ffc9d9d7132b41f779307d6092fca1c7d
jplara182001 <jplarar1@academia.usbbog.edu.co>
2025-05-05 17:39:31 +0000

    base

---
+++ src/test/java/api/ui/ui/ApiInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ui.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
